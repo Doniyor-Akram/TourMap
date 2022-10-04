479f52e3f0d2659e49615aaafa7418f8bdce043c
Doniyor <99491729+DoniyorAkramovich@users.noreply.github.com>
2022-10-04 07:25:34 +0500

    Initial Commit

---
+++ TourMap/TourMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourMapApp.swift
+//  TourMap
+//
+//  Created by Doniyor on 04/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TourMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
